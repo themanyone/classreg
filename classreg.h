c0c1fcbffed10afca2baff92d0ab3f5101bb01b2
Henry Kroll III <nospam@thenerdshow.com>
2016-03-11 18:58:42 -0800

    First Commit

---
+++ classreg.h
@@ -0,0 +1,89 @@
+/* classreg.h
+ * 
+ * Copyright (C) 2016 by Henry Kroll III, www.thenerdshow.com 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ */
+#define DEBUG 1
+#ifndef CLASSREG
+#define CLASSREG
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+//~ class prototypes
+void ClInstance (int methods,...);
+
+//~ exposed public methods
+enum {_CREATE, _DESTROY, _INFO, _APPEND, _INSERT};
+#define _STR(a) #a
+#define STR(a) _STR(a)
+//~ rudimentary error handling
+#define TRYTO(a) if (!(a)) { \
+    fprintf(stderr,"%s:%i: ",__FILE__,__LINE__); \
+    perror(#a); \
+    exit (1); }
+//~ Model Facades for Proxying in-line functions
+int _id;
+#define ForEach(s, t) for (_id=0;(t = s[_id]);_id++)
+#define split(b,c) for(_item=strtok(b,c);_item;(_item=strtok(NULL,c)))
+#define FREE(a) if(a){free(a);a=NULL; \
+    INFO(#a" freed.\n"); \
+    }
+#define INFO(...) if (DEBUG) \
+    fprintf(stderr,"info: " __VA_ARGS__)
+#define ERR(...) {\
+    fprintf(stderr,"err: " __VA_ARGS__); \
+    exit(1);}
+#define int_item *((int*)_item)
+#define char_item (char*)_item
+#define endl puts("");
+// a random integer within specified range (MSDN)
+#define getrandom(min, max) \
+    ((rand()%(int)(((max) + 1)-(min)))+ (min))
+//~ subclass the function object to make class objects
+#define class(a,...) a(int methods, __VA_ARGS__)
+#define use_classes void *_classes_used=NULL; void *_item;
+#define public va_list ap; va_start(ap,methods);switch
+#define method case
+#define param(a,b) b a=va_arg(ap,b);
+#define end_class va_end(ap);
+
+//~ subclass the return object for automatic cleanup!
+#define return if(_classes_used) { \
+    ClInstance(_DESTROY, &_classes_used); } return
+
+//~ Abstract insertion and destruction Method prototypes
+#define new_array(char,newarray,a,b) \
+    char **newarray=new_##char##_array(&_classes_used,a,b)
+#define destroy_array(char,newarray,a) \
+    destroy_##char##_array(newarray, 5);
+//~ Factory Method for reusable class templates
+#define new_array_factory(char)                                        \
+    void destroy_##char##_array (char **array, int d1) {               \
+        if (array) {                                                   \
+            for (int i=0;i<d1;i++) FREE (array[i]);                    \
+            FREE (array);                                              \
+    }   }                                                              \
+    char **new_##char##_array (void **_classes, int d1, int d2) {      \
+        char **newarray=malloc ((d1+1)*sizeof(char**));                \
+        ClInstance(_CREATE, _classes, newarray,                        \
+            destroy_##char##_array, d1);                               \
+        for (int i=0;i<d1;i++) {                                       \
+            newarray[i]=malloc (d2*sizeof(char*));                     \
+        } newarray[d1]=NULL;return newarray;                           \
+    }
+#endif
